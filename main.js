52b410b9ab75ceb9e9791cf50057954d8919fecd
haptanova03@gmail.com <haptanova03@gmail.com>
2024-04-22 23:13:36 +0800

    Добавление решения для этой задачи

--- main.js
+++ main.js
@@ -2,3 +2,25 @@
 принимающую на вход строку и возвращающую длину максимальной последовательности из неповторяющихся символов.
  Подстрока может состоять из одного символа. 
 Например в строке qweqrty, можно выделить следующие подстроки: qwe, weqrty. Самой длинной будет weqrty. */
+
+function getLongestLength(str) {
+    let longestLength = 0;
+    let currentSubstring = '';
+
+    for (let item of str) {
+        let itemIndex = currentSubstring.indexOf(item);
+
+        if (itemIndex === -1) {
+            currentSubstring += item;
+            longestLength = Math.max(longestLength, currentSubstring.length);
+        } else {
+            currentSubstring = currentSubstring.slice(itemIndex + 1) + item;
+        }
+    }
+
+    return longestLength;
+}
+
+console.log(getLongestLength('qweqwerty')); 
+console.log(getLongestLength('abdfehueily')); 
+console.log(getLongestLength('')); 
